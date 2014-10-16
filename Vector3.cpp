2ffd2bec3a9a90cdcb77f4711ac192d2f573a7f5
Kevin Zhang <ktzhang@ucsd.edu>
2014-10-15 23:44:01 -0700

    finished Vector3 methods

---
+++ Vector3.cpp
@@ -0,0 +1,53 @@
+#include "Vector3.h"
+#include "math.h"
+
+Vector3 Vector3::operator+(const Vector3& vector) {
+	this->x = vector.x + this->x;
+	this->y = vector.y + this->y;
+	this->z = vector.z + this->z;
+	return *this;
+}
+
+Vector3 Vector3::operator-(const Vector3& vector) {
+	this->x = vector.x - this->x;
+	this->y = vector.y - this->y;
+	this->z = vector.z - this->z;
+	return *this;
+}
+
+void Vector3::negate() {
+	this->x = -this->x;
+	this->y = -this->y;
+	this->z = -this->z;
+}
+
+void Vector3::scale(double s) {
+	this->x = this->x * s;
+	this->y = this->y * s;
+	this->z = this->z * s;
+}
+
+double Vector3::dot(const Vector3& vector1, const Vector3& vector2) { //: dot product, returns result
+	return vector1.x * vector2.x + 
+		vector1.y * vector2.y + 
+		vector1.z * vector2.z;
+}
+
+
+Vector3 Vector3::cross(const Vector3& vector1, const Vector3& vector2) {
+	return Vector3(
+		vector1.y*vector2.z - vector1.z*vector2.y,
+		vector1.z*vector2.x - vector1.x*vector2.z,
+		vector1.x*vector2.y - vector1.y*vector2.x);
+}
+
+double Vector3::length() {
+	return sqrt(this->x*this->x + this->y*this->y + this->z*this->z);
+}
+
+void Vector3::normalize() {
+	double length = this->length();
+	this->x = this->x / length;
+	this->y = this->y / length;
+	this->z = this->z / length;
+}
---
+++ Vector3.h
@@ -0,0 +1,35 @@
+#ifndef _VECTOR3_H_
+#define _VECTOR3_H_
+
+#define M_PI 3.14159265358979323846
+
+class Vector3
+{
+protected:
+	double m[4][4];   // Vector3 elements; first index is for rows, second for columns (row-major)
+
+public:
+	Vector3& operator=(const Vector3&);
+	double* getPointer();
+	void identity();
+	void transpose();
+	void makeRotateY(double);
+
+	double x;
+	double y;
+	double z;
+
+	Vector3::Vector3(double x, double y, double z) : x(x), y(y), z(z){};	// constructor with three parameters for the vector coordinates
+	Vector3 operator+(const Vector3& vector);			// overload operator '+' for addition
+	Vector3 operator-(const Vector3& vector); //overload operator '-' for subtraction
+
+	void negate(); //: negation
+	void scale(double s); //: scale(multiplication with scalar value)
+	double dot(const Vector3& vector1, const Vector3& vector2); //: dot product, returns result
+	Vector3 cross(const Vector3& vector1, const Vector3& vector2); //: cross product, returns result and puts it in calling vector
+	double length();// : length of the vector
+	void normalize();// : normalize the vector(make it so that its length is equal to one)
+	//void print(string comment);// : print x, y and z components of the vector after a comment string
+};
+
+#endif
