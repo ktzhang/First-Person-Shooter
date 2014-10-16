ada58211b722e69e6a3696e1a79b29998fcf7f17
Kevin Zhang <ktzhang@ucsd.edu>
2014-10-16 01:20:01 -0700

    finished vector4 class

--- Vector4.cpp
+++ Vector4.cpp
@@ -14,6 +14,13 @@ Vector4 Vector4::operator-(const Vector4& vector) {
 	return *this;
 }
 
+void Vector4::dehomogenize() {
+	for (int i = 0; i < Vector4::numElements-1; i++){
+		this->m[i] = this->m[i] / this->m[numElements];
+	}
+	this->m[numElements] = 1;
+}
+
 void Vector4::print(std::string& comment) {
 	comment.append("x: " + std::to_string(this->m[0]) + ", y: " + std::to_string(this->m[1]) + ", z: " + std::to_string(this->m[2]) + ", w: " + std::to_string(this->m[3]));
 }
